d4266225979ff96d79d04f20be91348bc1fc9f7f
athy125 <atharvajosh369@gmail.com>
2021-02-28 22:46:37 +0530

    Created,styled and functionalised Filtersearch and FilterList component

---
+++ client/src/components/elements/FilterSearch/FilterSearch.less
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import css from './filterSearch.less';
+
+const FilterSearch = props => (
+  <div className={css.search}>
+    <input className={css.search__input} type="text" placeholder="Search..." onChange={props.onChange} />
+    <button className={css.search__icon} onClick={props.onClick} disabled={!props.value} />
+  </div>
+);
+
+FilterSearch.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired
+};
+
+export default FilterSearch;
